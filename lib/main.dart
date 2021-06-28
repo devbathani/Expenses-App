e36f3b34dcb016a78d08a30765e493f45065a055
DEV BATHANI <76676470+devbathani@users.noreply.github.com>
2021-06-28 14:55:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,16 @@ import 'package:expenses/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expenses/models/transaction.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.landscapeLeft,
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  //   DeviceOrientation.landscapeRight
+  // ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -36,6 +45,8 @@ class _MyHomePageState extends State<MyHomePage> {
       return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
     }).toList();
   }
+
+  bool _showchart = false;
 
   void _addTransaction(String txTitle, double txAmount, DateTime chosenDate) {
     final newTx = Transaction(
@@ -70,24 +81,62 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Expenses",
-          style: TextStyle(fontFamily: 'OpenSans'),
-        ),
-        actions: [
-          IconButton(
-              onPressed: () => _startToAddTx(context), icon: Icon(Icons.add))
-        ],
+    final islandScape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    final appBar = AppBar(
+      title: Text(
+        "Expenses",
+        style: TextStyle(fontFamily: 'OpenSans'),
       ),
+      actions: [
+        IconButton(
+            onPressed: () => _startToAddTx(context), icon: Icon(Icons.add))
+      ],
+    );
+    final txListWidget = Container(
+        height: (MediaQuery.of(context).size.height -
+                appBar.preferredSize.height -
+                MediaQuery.of(context).padding.top) *
+            0.75,
+        child: TransactionList(_usertransactions, _deleteTransaction));
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           //mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Chart(_recentTransaction),
-            TransactionList(_usertransactions, _deleteTransaction),
+            if (islandScape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Show Chart"),
+                  Switch(
+                      value: _showchart,
+                      onChanged: (val) {
+                        setState(() {
+                          _showchart = val;
+                        });
+                      })
+                ],
+              ),
+            if (!islandScape)
+              Container(
+                  height: (MediaQuery.of(context).size.height -
+                          appBar.preferredSize.height -
+                          MediaQuery.of(context).padding.top) *
+                      0.3,
+                  child: Chart(_recentTransaction)),
+            if (!islandScape) txListWidget,
+            if (islandScape)
+              _showchart
+                  ? Container(
+                      height: (MediaQuery.of(context).size.height -
+                              appBar.preferredSize.height -
+                              MediaQuery.of(context).padding.top) *
+                          0.4,
+                      child: Chart(_recentTransaction))
+                  : txListWidget,
           ],
         ),
       ),
@@ -100,3 +149,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
